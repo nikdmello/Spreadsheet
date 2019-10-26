9f2ae025372a1a3d1fe58e4cb71ccbf62836f785
joaquinfrommer <joaquinfrommer@gmail.com>
2019-10-26 18:28:58 -0400

    Initial classes and interfaces

--- src/edu/cs3500/spreadsheets/model/Value.java
+++ src/edu/cs3500/spreadsheets/model/Value.java
@@ -4,6 +4,12 @@ package edu.cs3500.spreadsheets.model;
  * Container for any type of data value such a double, boolean, or string.
  */
 public interface Value extends Formula {
+
+  /**
+   * Evaluates the value as itself because values evaluate to themselves
+   * @return this Value
+   */
   Value evaluate();
+
   <R> R accept(ValueVisitor<R> visitor);
 }
