05e3970261bc4887b29f6453927db77ae00e2fdd
nikdmello <nik.dmello@gmail.com>
2019-10-26 18:28:52 -0400

    Add javadoc

--- src/edu/cs3500/spreadsheets/model/Value.java
+++ src/edu/cs3500/spreadsheets/model/Value.java
@@ -5,5 +5,13 @@ package edu.cs3500.spreadsheets.model;
  */
 public interface Value extends Formula {
   Value evaluate();
+
+  /**
+   * Accepts a value visitor and returns R
+   * @param visitor
+   * @return the desired value R
+   */
   <R> R accept(ValueVisitor<R> visitor);
 }
+
+
